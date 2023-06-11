c12472556c5c87ae13b3a0c43f48174df81c888e
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-06-11 18:36:47 +0800

    Update 57_[指定x和y范围]自由能曲面及二维填色图绘制.py

--- 57_[指定x和y范围]自由能曲面及二维填色图绘制.py
+++ 57_[指定x和y范围]自由能曲面及二维填色图绘制.py
@@ -14,16 +14,36 @@ from scipy.interpolate import griddata
 # 通常在 100 到 300 DPI 之间选择一个合适的值即可。如果需要更高的分辨率，可以考虑使用矢量格式的图像，如 PDF、SVG 等，它们不受 DPI 的限制，可以随意缩放而不会失去清晰度。
 plt.rcParams['figure.dpi'] = 600
 
+
+print("""
+      请结合具体情况依次修改如下部分代码参数:
+          
+      data = np.loadtxt('editColvar.txt')
+      xmin, xmax =    -5, 36        #  B-Si, -5, 31.5
+      ymin, ymax =      -5, 10      #  B-O, -5, 10
+      zmin, zmax = -15000, 0        #  筛选z轴数据
+      
+      offset = -3000                #  填色图在z轴位置
+      ax.set_zlim(-3000, 0)         # # 设置z轴坐标范围
+      
+      ax.set_xlabel('B-Si coordination number')   # x轴标题
+      ax.set_ylabel('B-O coordination number')    # y轴标题
+      ax.set_zlabel('Energy (kJ/mol)')            # z轴标题
+      
+      """)
+
 # 加载数据
-data = np.loadtxt('fes.dat')
+data = np.loadtxt('editColvar.txt')   # 
 x = data[:, 0]
 y = data[:, 1]
 z = data[:, 2]*4.3597*6.022*100
 
 # 定义x轴和y轴范围
-xmin, xmax =    -5, 31.5      #  B-Si, -5, 31.5
+xmin, xmax =    -5, 36        #  B-Si, -5, 31.5
 ymin, ymax =      -5, 10      #  B-O, -5, 10
 zmin, zmax = -15000, 0
+offset = -3500                #  填色图在z轴位置
+
 
 # 根据指定范围筛选数据
 idx = (xmin <= x) & (x <= xmax) & (ymin <= y) & (y <= ymax) & (zmin <= z) & (z <= zmax)
@@ -37,10 +57,14 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_trisurf(x, y, z, cmap='viridis')
 
 # 添加标签和标题
-ax.set_xlabel('B-Si coordination number')
-ax.set_ylabel('B-O coordination number')
+ax.set_xlabel('Z-axis component')
+ax.set_ylabel('Mg-O coordination number')
 ax.set_zlabel('Energy (kJ/mol)')
 ax.set_title('3-dimension contour and surface plot')
+
+# 设置z轴坐标范围
+ax.set_zlim(-3500, 0) 
+
 
 # 定义网格
 xi = np.linspace(min(x), max(x)+0.5, 500)
@@ -51,7 +75,7 @@ X, Y = np.meshgrid(xi, yi)
 Z = griddata((x, y), z, (X, Y), method='linear')
 
 # 绘制投影图
-contour = ax.contourf(X, Y, Z, cmap='viridis', levels=40, offset=-9500)
+contour = ax.contourf(X, Y, Z, cmap='viridis', levels=40, offset=offset)
 
 # 添加colorbar
 cbar_ax = fig.add_axes([0.88, 0.10, 0.02, 0.7])
